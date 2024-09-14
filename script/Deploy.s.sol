aaaff2cdea9d022c774ce3ef6f8e824261297f6e
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-09-14 21:46:42 +0530

    chore: Initial project setup

---
+++ script/Deploy.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+
+contract Deploy is Script {
+    function run() public {
+        vm.startBroadcast();
+
+        vm.stopBroadcast();
+    }
+}
